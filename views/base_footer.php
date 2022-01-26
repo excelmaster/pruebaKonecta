9dccfb53f0a8054011bbaee83037205bfd96579d
betfie1@gmail.com <betfie1@gmail.com>
2022-01-26 18:36:34 -0500

    ajuste templates

--- views/base_footer.php
+++ views/base_footer.php
@@ -8,9 +8,9 @@
 </div>
 <script type="text/javascript" src="./misc/jquery/jquery.min.js"></script>
 <script type="text/javascript" src="./misc/bootstrap/bootstrap.min.js"></script>
-<script type="text/javascript" src="./js/users/users.js"></script>
+<script type="text/javascript" src="./js/productos/productos.js"></script>
 <script type="text/javascript">
-	objUser=new Users();
+	//objUser=new Users();
 </script>
 </body>
 </html>
--- views/base_header.php
+++ views/base_header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="content-language" content="es">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php echo $title=isset($title)?$title:'CRUD con PHP, MySql y Bootstrap'; ?></title>
+	<title><?php echo $title=isset($title)?$title:'Prueba_Konecta'; ?></title>
 	<link rel="stylesheet" type="text/css" href="./misc/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="./misc/style.css">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
