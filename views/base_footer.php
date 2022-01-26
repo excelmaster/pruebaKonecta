6f290e1675935f695ee7ab547fcf94e700d2f890
betfie1@gmail.com <betfie1@gmail.com>
2022-01-26 16:23:23 -0500

    templates view ok, miscelanea y codigo JS

---
+++ views/base_footer.php
@@ -0,0 +1,16 @@
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col text-center">
+			<p>Prueba php</p>			
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="./misc/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="./misc/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="./js/users/users.js"></script>
+<script type="text/javascript">
+	objUser=new Users();
+</script>
+</body>
+</html>
