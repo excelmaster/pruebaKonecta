72ff7a5dac2eb20c77043ff8b5a15bcfff5e8a01
betfie1@gmail.com <betfie1@gmail.com>
2022-01-26 20:22:07 -0500

    actualiza script db

--- db/script_db.sql
+++ db/script_db.sql
@@ -25,6 +25,46 @@ CREATE TABLE `lista_ventas` (
 	`fecha` DATETIME NOT NULL
 ) ENGINE=MyISAM;
 
+-- Volcando estructura para tabla konectacafe.productos
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(250) NOT NULL,
+  `referencia` varchar(250) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `peso` int(11) NOT NULL,
+  `categoria` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `create_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- Volcando datos para la tabla konectacafe.productos: ~2 rows (aproximadamente)
+DELETE FROM `productos`;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` (`id`, `nombre`, `referencia`, `precio`, `peso`, `categoria`, `stock`, `create_date`) VALUES
+	(1, 'ducales', 'csg999', 777, 56, 'saladas', 6598, '2022-01-26'),
+	(2, 'festival', 'csg', 500, 80, 'galletas', 10, '2022-01-26');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+
+-- Volcando estructura para tabla konectacafe.ventas
+CREATE TABLE IF NOT EXISTS `ventas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK1_producto` (`id_producto`),
+  CONSTRAINT `FK1_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+-- Volcando datos para la tabla konectacafe.ventas: ~2 rows (aproximadamente)
+DELETE FROM `ventas`;
+/*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
+INSERT INTO `ventas` (`id`, `id_producto`, `cantidad`, `fecha`) VALUES
+	(4, 1, 25, '2022-01-26 20:02:26'),
+	(5, 2, 1, '2022-01-26 20:02:43');
+/*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
+
 -- Volcando estructura para vista konectacafe.lista_ventas
 -- Eliminando tabla temporal y crear estructura final de VIEW
 DROP TABLE IF EXISTS `lista_ventas`;
